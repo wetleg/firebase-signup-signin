877b922f70932ea49323f102997d32396475a5c1
nigelkelly <nigekelly@gmail.com>
2013-09-12 17:21:59 +0100

    step 3  firebase signup auth

--- js/SignUpViewModel.js
+++ js/SignUpViewModel.js
@@ -1,12 +1,42 @@
 var SignUpViewModel = function() {
+	
+	var firebaseRoot = new Firebase("https://flat-tasks.firebaseio.com"); 
 
+	var authClient = new FirebaseSimpleLogin(firebaseRoot, function(error, user) {
+	 	if (error) {
+	    	// an error occurred while attempting login
+	    	console.log(error);
+			alert("User name or password is not correct. Please try again.");
+	  	} else if (user) {
+	    	// user authenticated with Firebase
+	    	console.log('Logging In User ID: ' + user.id + ', Provider: ' + user.provider);  
+	    	
+	  	} else {
+	    	// user is logged out
+	    	console.log('User is logged out');
+	  	}
+	});
+	
+	
 	var self = this;
 	
 	self.userName = ko.observable();
 	self.userPassword = ko.observable();
 	
 	self.signup = function() {
-		console.log( "signing up "+self.userName() );
+		
+		console.log( "Signing up "+self.userName() );
+		
+		authClient.createUser(self.userName(), self.userPassword(), function(error, user) {
+		 	
+			if (!error) {
+				console.log( "Signed up "+self.userName() );
+		    	console.log('Firebase User Id: ' + user.id + ', and Email: ' + user.email);
+			
+		  	} else {
+				alert( error.message +" Please login. Thank you.");
+			}
+		});
 		
 	}
 
