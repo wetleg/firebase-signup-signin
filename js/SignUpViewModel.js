fd2bb5de2818546a3e2175a7b89e2cf82780ae4a
nigelkelly <nigekelly@gmail.com>
2013-09-12 17:40:19 +0100

    step 3  firebase signup auth

--- js/SignUpViewModel.js
+++ js/SignUpViewModel.js
@@ -18,12 +18,14 @@ var SignUpViewModel = function() {
 		console.log( "Signing up "+self.userName() );
 		
 		authClient.createUser(self.userName(), self.userPassword(), function(error, user) {
-		 	
+		 	// do signup authentication checks here
 			if (!error) {
+				// User not signed up so .... sign her up
 				console.log( "Signed up "+self.userName() );
 		    	console.log('Firebase User Id: ' + user.id + ', and Email: ' + user.email);
 			
 		  	} else {
+				// User already signed up
 				alert( error.message +" Please login. Thank you.");
 			}
 		});
